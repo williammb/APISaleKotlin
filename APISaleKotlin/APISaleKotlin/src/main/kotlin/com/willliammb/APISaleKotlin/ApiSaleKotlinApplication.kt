703c7c1ece70a63bd5d5f17a07fbca752714e23b
williammb <wmb96@hotmail.com>
2022-01-01 11:31:14 -0300

    Init

---
+++ APISaleKotlin/APISaleKotlin/src/main/kotlin/com/willliammb/APISaleKotlin/ApiSaleKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.willliammb.APISaleKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiSaleKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiSaleKotlinApplication>(*args)
+}
---
+++ APISaleKotlin/APISaleKotlin/src/test/kotlin/com/willliammb/APISaleKotlin/ApiSaleKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.willliammb.APISaleKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiSaleKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
